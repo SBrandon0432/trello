79c1ec8a1d8efb124d14ecb5e45eb8b221867758
sbrandon0432 <sbrandon0432@yahoo.com>
2022-08-16 16:46:45 -0700

    file tree update

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="react-scripts" />
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.mp4";
+declare module "*.mp3";
+declare module "*.webm";
+declare module "*.pdf";
+declare module "*.gltf";
