817e0a8fc96f253f7a22d558bf5439e17eb60405
sbrandon0432 <sbrandon0432@yahoo.com>
2022-08-18 16:47:44 -0700

    trillo site start:

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,18 +2,7 @@
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="item"> 1 </div>
-        <div className="item i2"> 2 </div>
-        <div className="item i3"> 3 </div>
-        <div className="item i4"> 4 </div>
-        <div className="item"> 5 </div>
-        <div className="item"> 6 </div>
-        <div className="item"> 7 </div>
-        <div className="item"> 8 </div>
-        <div className="item"> 9 </div>
-        <div className="item"> 10 </div>
-      </div>
+
     </div>
   );
 }
