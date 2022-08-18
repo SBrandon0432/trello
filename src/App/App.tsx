047a4524be1638a7f8c46f77118dacad569f4712
sbrandon0432 <sbrandon0432@yahoo.com>
2022-08-18 14:53:19 -0700

    flex end i4

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,7 +6,7 @@ function App() {
         <div className="item"> 1 </div>
         <div className="item i2"> 2 </div>
         <div className="item"> 3 </div>
-        <div className="item"> 4 </div>
+        <div className="item i4"> 4 </div>
         <div className="item"> 5 </div>
       </div>
     </div>
