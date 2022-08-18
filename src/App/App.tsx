e3352c74c54ecdfaf425e853b78c91703e6d7bc6
sbrandon0432 <sbrandon0432@yahoo.com>
2022-08-18 16:13:26 -0700

    flex wrap

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,6 +8,11 @@ function App() {
         <div className="item i3"> 3 </div>
         <div className="item i4"> 4 </div>
         <div className="item"> 5 </div>
+        <div className="item"> 6 </div>
+        <div className="item"> 7 </div>
+        <div className="item"> 8 </div>
+        <div className="item"> 9 </div>
+        <div className="item"> 10 </div>
       </div>
     </div>
   );
