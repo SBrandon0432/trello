5616376a0ba70be27b516fde4efde6fe0f99b807
sbrandon0432 <sbrandon0432@yahoo.com>
2022-08-18 14:56:40 -0700

    flex box item order

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,7 +5,7 @@ function App() {
       <div className="container">
         <div className="item"> 1 </div>
         <div className="item i2"> 2 </div>
-        <div className="item"> 3 </div>
+        <div className="item i3"> 3 </div>
         <div className="item i4"> 4 </div>
         <div className="item"> 5 </div>
       </div>
