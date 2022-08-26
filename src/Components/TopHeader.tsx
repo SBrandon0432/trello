ad4f722ce0cb10eecadaa1689efd97c50a57ef04
sbrandon0432 <sbrandon0432@yahoo.com>
2022-08-25 19:02:18 -0700

    search bar and button

--- src/Components/TopHeader.tsx
+++ src/Components/TopHeader.tsx
@@ -6,6 +6,12 @@ const TopHeader = () => {
   return (
     <header className="header">
       <img src={logo} alt='logo' className='logo'/>
+      <form action="#" className='search'>
+        <input className='search__input' placeholder='Search Hotels'/>
+        <button className='search__button'>
+          Search
+        </button>
+      </form>
     </header>
   )
 }
