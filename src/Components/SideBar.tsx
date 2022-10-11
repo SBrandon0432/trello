39ca692722f3d505ad874a517ed28d849cbec17b
sbrandon0432 <sbrandon0432@yahoo.com>
2022-10-11 15:33:16 -0700

    htlm static side bar, and side bar nav svg icon

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -1,10 +1,28 @@
+import iconSprite from '../Assets/img/sprite.svg'
+
 
 const SideBar = (props: any) => {
 
 
   return(
     <nav className="sidebar">
-     side bar Navigation
+      <ul className="side-nav">
+        <li className="side-nav__item">
+          <a href="/#" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${iconSprite}#icon-home`}></use>
+            </svg>
+          </a>
+        </li>
+      </ul>
+
+
+
+
+
+
+
+
     </nav>
   )
 }
