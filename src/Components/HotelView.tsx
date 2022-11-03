8d8a8d9e7fd2a3be52d4ed4555a52f88f14afeb6
sbrandon0432 <sbrandon0432@yahoo.com>
2022-11-03 17:51:40 -0400

    image import for 1-3 hotel view

--- src/Components/HotelView.tsx
+++ src/Components/HotelView.tsx
@@ -1,10 +1,27 @@
+import image1 from '../Assets/img/hotel-1.jpg'
+import image2 from '../Assets/img/hotel-2.jpg'
+import image3 from '../Assets/img/hotel-3.jpg'
+
 
 const Main = () => {
 
 
   return (
     <main className="hotel-view">
-      HotelView
+      <div className="gallery">
+        <figure className="gallery__item">
+          <img src={image1}/>
+        </figure>
+      </div>
+
+
+
+
+
+
+
+
+
     </main>
   )
 }
