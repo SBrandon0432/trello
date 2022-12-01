a5512d401524b27c3c2802d577203e58e5e235c5
sbrandon0432 <sbrandon0432@yahoo.com>
2022-12-01 15:07:29 -0500

    hotel description component

---
+++ src/Components/HotelDescription.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+const HotelDescription = (props: any) => {
+
+
+  return (
+    <div className="description">
+
+    </div>
+  )
+
+}
+
+export default HotelDescription;
